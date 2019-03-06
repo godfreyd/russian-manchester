0c2ecdb53a3e6fa3b6eb75d26cea3a2373ce7c61
Sergei Bocharov <sergei-b84@bk.ru>
2019-03-06 23:40:23 +0300

    Started findList method

--- v1/textile.js
+++ v1/textile.js
@@ -3,6 +3,46 @@ const Textile = require('models/textile');
 
 module.exports = {
 
+    findList: async (req, res) => {
+
+        const materials = res.locals.queryHelper.getArray('materials');
+        const colors = res.locals.queryHelper.getArray('colors');
+        const brands = res.locals.queryHelper.getArray('brands');
+        const categorias = res.locals.queryHelper.getArray('categorias');
+        const sizes = res.locals.queryHelper.getArray('sizes');
+        const { text, priceMin, priceMax } = req.query;
+        const words = text && _.words(text, /\S+/g);
+
+        _.each(categorias, item => assert.category(item));
+        _.each(sizes, item => assert.size(item));
+        _.each(brands, item => item && assert.identity(item, 'Brand name'));
+        _.each(materials, item => assert.material(item));
+        _.each(colors, item => assert.color(item));
+        /* eslint-disable no-unused-expressions */
+        _.isUndefined(priceMin) || assert.float(priceMin, 'Min price');
+        _.isUndefined(priceMax) || assert.float(priceMax, 'Max price');
+        /* eslint-enable no-unused-expressions */
+
+        const filters = {
+            categorias,
+            materials,
+            brands,
+            priceMin,
+            priceMax,
+            sizes,
+            words,
+            colors
+        };
+
+        const devices = await Device.findList(filters, { words });
+
+        const body = _(devices)
+            .map(serializer('devicesItem'))
+            .values();
+
+        res.send(body);
+    },
+
     save: async (req, res) => {
         const textileData = req.body;
 
